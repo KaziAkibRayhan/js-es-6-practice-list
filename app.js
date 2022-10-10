aabc4d6f08e60d10c4e5ccd3f387a8b708cb7251
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-10-10 12:08:53 +0600

    let const templete string

---
+++ app.js
@@ -0,0 +1,20 @@
+//  1  let const 
+const myName = 'Akib Rayhan';
+let year = 2022;
+
+const todos = {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+};
+
+// 2 templete string 
+
+const sentence = `
+ Hey my name is: ${myName}
+ And I wish this ${year}.I am a wev developer.
+ My Id is : ${todos.userId}.
+ My Designation: ${todos.title}
+`;
+// console.log(sentence);
